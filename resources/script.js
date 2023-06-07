23211ffda2dcd89530efe4d7b4329bca0d7583f0
webVerts <webverts@protonmail.com>
2023-06-07 13:49:12 +0530

    Minor Link Change The previous `Read how we verify what is valid and what is not.` used to reidrect to url-frontend repo. With this fix it will redirect to fame-frontend repo

--- resources/script.js
+++ resources/script.js
@@ -173,7 +173,7 @@ for (var i = 0; i < cl.length; i++) {
   if (json.urls[i] === link) {
     var Data = `<div style="color:white;font-size:12px; background-color:#1f282d;">` + new_link + `</br> <p> <span style="color:#A2FB15; font-size: 14px; ">Verified by authifyURL.</span> &nbsp;<span class="tooltip" > ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span style="font-size:14px; color:white;">The page you submitted belongs to: </span><br> <span class="op_logo"><img src="${json.logo}"></span><span style="font-size:18px; color: #DFb014"> ${json.name}</span></p></br></div>`;
                 
-    var Disclaimer = `Read <a href="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white"; target="_blank"> how we verify</a> what is valid and what is not. </br>`;
+    var Disclaimer = `Read <a href="https://github.com/authifyWeb/fame-Frontend#how-we-verify" style="color:white"; target="_blank"> how we verify</a> what is valid and what is not. </br>`;
 
     data.innerHTML = Data;
     disclaimer.innerHTML = Disclaimer;
@@ -187,7 +187,7 @@ for (var i = 0; i < cl.length; i++) {
 if (!conditionMet) {
   var Data = `<div style="color:white; background-color:#1f282d; font-size:12px;" >${new_link}<p><br><span style="color:red;  font-size: 18px;"> The page you submitted doesn't belong to the organization selected above</span>&nbsp; <span class="tooltip"> ❌ <span class="tooltiptext">Possibly a scam. Report the page directly to the owner. </span> </p> </div>`;
 
-  var Disclaimer = `<br>Read <a href="https://github.com/authifyWeb/authifyURL#how-we-verify" style="color:white;" target="_blank"> how we verify</a> what is valid and what is not. </br>`;
+  var Disclaimer = `<br>Read <a href="https://github.com/authifyWeb/fame-Frontend#how-we-verify" style="color:white;" target="_blank"> how we verify</a> what is valid and what is not. </br>`;
 
   data.innerHTML = Data;
   disclaimer.innerHTML = Disclaimer;
