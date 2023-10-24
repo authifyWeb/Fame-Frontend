b2d6009c3e1f488495a7f92f591f153d705ac4aa
webVerts <100699190+webVerts@users.noreply.github.com>
2023-10-24 14:57:42 +0530

    Update script.js
    Typo

--- resources/script.js
+++ resources/script.js
@@ -191,7 +191,7 @@ var conditionMet = false; // Flag variable to track if the condition is met
 for (var i = 0; i < cl.length; i++) {
   if (json.urls[i] === link) {
 
-    var Data = `<div style="color:white;font-size:12px; background-color:#1f282d;">` + display_link + `</br> <p> <span style="color:#A2FB15; font-size: 14px; ">Verified by authifyURL.</span> &nbsp;<span class="tooltip" > ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span style="font-size:14px; color:white;">The page you submitted belongs to: </span><br> <span class="op_logo"><img src="${json.logo}"></span><span style="font-size:18px; color: #DFb014"> ${json.name}</span></p></br></div>`;
+    var Data = `<div style="color:white;font-size:12px; background-color:#1f282d;">` + display_link + `</br> <p> <span style="color:#A2FB15; font-size: 14px; ">Verified by authifyFame.</span> &nbsp;<span class="tooltip" > ✅ <span class="tooltiptext">This website is valid and legal. </span> </p></br> <p><span style="font-size:14px; color:white;">The page you submitted belongs to: </span><br> <span class="op_logo"><img src="${json.logo}"></span><span style="font-size:18px; color: #DFb014"> ${json.name}</span></p></br></div>`;
 
                 
     var Disclaimer = `Read <a href="https://github.com/authifyWeb/fame-Frontend#how-we-verify" style="color:white"; target="_blank"> how we verify</a> what is valid and what is not. </br>`;
