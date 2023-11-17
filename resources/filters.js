660476d55c954f70d33492cbe6560da546050921
webVerts <webverts@protonmail.com>
2023-11-17 21:52:59 +0530

    Add liinks.co

--- resources/filters.js
+++ resources/filters.js
@@ -239,6 +239,13 @@ export function filtering(url, href, origin, hostname,protocol,pathname,search,d
       var output = compare(link,href);
       return output;
     }
+    else if(hostname="www.liinks.co"){
+      var path1=pathname.split('/')[1].toLowerCase();
+      link=hostname+'/'+path1;
+      var output =compare(link,href);
+      console.log(link);
+      return output;
+    }
 
   else if(hostname == "addons.mozilla.org")
         /*Once the above condition is true, the function replaces the URL language to the the default en-US. This is used since mozilla supports multiple languages and the url structure is directly based on user language.  */
